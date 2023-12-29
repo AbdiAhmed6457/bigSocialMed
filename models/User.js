78c67a28b74de3969b71d8b0c6510a6ad4a022c1
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2023-12-28 22:04:53 -0800

    first node for full

---
+++ models/User.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        min:3,
+        max:20,
+        unique:true
+    }, 
+    email: {
+        type: String,
+        required: true,
+        max: 50,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6
+    },
+    profilepicture:{
+        type: String,
+        default: ""
+    },
+    coverpicture:{
+        type: String,
+        default: ""
+    },
+    followers: {
+        type: Array,
+        default: []
+    },
+    following: {
+        type: Array,
+        default: []
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false
+    },
+    desc:{
+        type: String,
+        max:50
+    },
+    city: {
+        type:String,
+        max:50
+    },
+    relationship: {
+        type: String,
+        enum: [1,2,3,4]
+    }
+
+},
+{timestamps: true}
+
+);
+
+module.exports = mongoose.model("User", UserSchema)
