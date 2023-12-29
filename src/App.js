e96559855f2f1047b5563bc72c7620bc7b735a62
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2023-12-28 22:01:01 -0800

    first for full

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import Register from "./pages/Register/Register";
+import Home from "./pages/home/Home";
+// import Login from "./pages/login/Login";
+// import Profile from "./pages/profile/Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      {/* <Register/> */}
+      
+    </div>
+  );
+}
+
+export default App;
